22907acc99a8006e3dba1266c9dd6ab2b3c81310
Kirsten Opstad <kirsten.opstad@gmail.com>
2023-02-24 13:59:29 -0800

    add basic footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,17 @@
 import React from "react";
+import Nav from 'react-bootstrap/Nav';
+
 
 const Footer = () => {
   return(
     <React.Fragment>
-      <h1>Footer</h1>
+      <div className="footer">
+        <Nav defaultActiveKey="/home" className="flex-column">
+          <Nav.Link href="/home" disabled>About MOFA</Nav.Link>
+          <Nav.Link eventKey="link-1" disabled>FAQ</Nav.Link>
+          <Nav.Link eventKey="link-2" disabled>Subscribe</Nav.Link>
+        </Nav>
+      </div>
     </React.Fragment>
   );
 }
