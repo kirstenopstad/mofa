d9ef65b8f2c53880545fa1e13bda48866f401671
Kirsten Opstad <kirsten.opstad@gmail.com>
2023-02-24 08:20:04 -0800

    refactor to remove class component and redux store

--- src/components/ExhControl.js
+++ src/components/ExhControl.js
@@ -4,44 +4,35 @@ import PropTypes from "prop-types";
 import ArtworkDetail from "./ArtworkDetail";
 import ExhDetail from "./ExhDetail";
 import ExhList from "./ExhList";
+import seedData from './../ExhSeedData'
+
 
 // TODO: turn this back into const function
-class ExhControl extends React.Component {
+const ExhControl = () => {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      // this makes eslint happy - no-useless-constructors
-    }
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     // this makes eslint happy - no-useless-constructors
+  //   }
+  // }
 
-  render() {
-
-    return(
-      // TODO: add logic to display all exhibits or just one
-      <React.Fragment>
-        <ExhList exhibitions={this.props.exhibitions}/>
-        <ExhDetail />
-        <ArtworkDetail />
-    </React.Fragment>
-    );
-  }
+  return(
+    // TODO: add logic to display all exhibits or just one
+    <React.Fragment>
+      <ExhList exhibitions={seedData}/>
+      <ExhDetail />
+      <ArtworkDetail />
+  </React.Fragment>
+  );
 }
 
 
-// Map to props!
-const mapStateToProps = state => {
-  return {
-    exhibitions: state.exhibitions,
-  }
-}
 
 // Proptypes!
 ExhControl.propTypes = {
   exhibitions: PropTypes.object,
 }
 
-// Connect wrapper
-ExhControl = connect(mapStateToProps)(ExhControl)
 
 export default ExhControl;
