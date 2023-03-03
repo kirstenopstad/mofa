b8ef19e221406c27712f3f4e93d35d1240db79b2
Kirsten Opstad <kirsten.opstad@gmail.com>
2023-03-03 08:49:04 -0800

    style exhibit detail to match wireframe

--- src/components/ExhDetail.js
+++ src/components/ExhDetail.js
@@ -2,6 +2,8 @@ import React from "react";
 import Artwork from "./Artwork";
 import { PropTypes } from "prop-types";
 import closeIcon from './../img/icons/x-lg.svg'
+import Container from "react-bootstrap/Container";
+import Row from 'react-bootstrap/Row';
 
 const ExhDetail = ({ exh, art, titles, onArtworkDetailClick, onClose}) => {
   const {exhTitle, endDate, description, id } = exh;
@@ -15,21 +17,21 @@ const ExhDetail = ({ exh, art, titles, onArtworkDetailClick, onClose}) => {
   return(
     // add seed data 
     // display all artwork in a single given exhibition
-    <React.Fragment>
+    <Container>
       <img onClick={() => onClose()} src={closeIcon}/>
-      <h2>Selected Exhibition</h2>
-      <h3>{exhTitle}</h3>
-      <h4>Ends {endDate}</h4>
+      <h2>{exhTitle}</h2>
+      <h4><i>Ends {endDate}</i></h4>
       <p>{description}</p>
       {filteredArtwork.map((art) => 
+      <Row className="exh">
           <Artwork 
-            key={`artwork-${art.id}`} 
-            selArt={art} 
-            titles={titles}
-            whenArtworkClick={onArtworkDetailClick}/>
-      )}
-      <br />
-    </React.Fragment>
+          key={`artwork-${art.id}`} 
+          selArt={art} 
+          titles={titles}
+          whenArtworkClick={onArtworkDetailClick}/>
+      </Row>    
+          )}
+    </Container>
   );
 }
 
