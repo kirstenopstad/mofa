9958e9006c0430cc9e38d6ad41a6fc1cc847fe2c
Kirsten Opstad <kirsten.opstad@gmail.com>
2023-03-03 08:33:13 -0800

    style exhibit in list to match wireframe

--- src/components/Exhibit.js
+++ src/components/Exhibit.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { PropTypes } from "prop-types";
 import Artwork from "./Artwork";
+import Container from "react-bootstrap/Container";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 const Exhibit = ({exhibit, artworks, whenExhDetailClicked}) => {
   const {exhTitle, endDate, description, id } = exhibit;
@@ -10,17 +14,28 @@ const Exhibit = ({exhibit, artworks, whenExhDetailClicked}) => {
   // shows images connected to *this* exh only
   const filteredArtwork = artworks.filter(a => a.exhId === id)
 
+  // get featured image
+  const featuredArtwork = filteredArtwork[0]
+
   return(
     <React.Fragment>
-      <h3>{exhTitle}</h3>
-      <h4>Ends {endDate}</h4>
-      <p>{description}</p>
-      {filteredArtwork.map((art) =>
-        <img key={`exh-img-${art.id}`}src={art.image} alt={art.prompt} className="exh-preview"/>
-      )}
-      <br />
-      {/* TODO: Conditional rendering, is clicked show ExhDetail */}
-      <button onClick={() => whenExhDetailClicked(id)}>Exhibition Details</button>
+      <Container>
+      <Row className="exh">
+        <Col sm={8}>
+          <img src={featuredArtwork.image} alt={featuredArtwork.prompt} className="exh-preview"/>
+        </Col>
+        <Col sm={4} className="exh-didactic">
+          <h3><b>{exhTitle}</b></h3>
+          <h5><i>Ends {endDate}</i></h5>
+          <p>{description}</p>
+          <Button variant="outline-dark" onClick={() => whenExhDetailClicked(id)}>Exhibition Details</Button>
+        </Col>
+        {/* {filteredArtwork.map((art) =>
+          <img key={`exh-img-${art.id}`}src={art.image} alt={art.prompt} className="exh-preview"/>
+        )} */}
+
+      </Row>
+      </Container>
     </React.Fragment>
   );
 }
