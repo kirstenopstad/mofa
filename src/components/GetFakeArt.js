43968008ccadeb1dabace70a0185fae33921b16b
Kirsten Opstad <kirsten.opstad@gmail.com>
2023-03-05 10:30:02 -0800

    WIP: persistent CORS issues with direct OpenAI call

--- src/components/GetFakeArt.js
+++ src/components/GetFakeArt.js
@@ -17,9 +17,7 @@ const GetFakeArt = () => {
   const [error, setError]= useState(null);
   const [isLoaded, setIsLoaded]= useState(false);
   const [fakeArtURL, setFakeArtURL]= useState("");
-
-  // const URL="https://oaidalleapiprodscus.blob.core.windows.net/private/org-IyCMwj0RWWvhDkV14UomDgos/user-9EsOmgI1HmI0SzBiND2o5SSU/img-7thX2cgJRDyyPIvqgM49amkG.png?st=2023-03-05T16%3A43%3A10Z&se=2023-03-05T18%3A43%3A10Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-03-05T01%3A26%3A38Z&ske=2023-03-06T01%3A26%3A38Z&sks=b&skv=2021-08-06&sig=b/z1K1qXPQfJDqv2AL%2BHRQH5Q8U5sYfSCout911XChg%3D"
-  
+    
   // API call via AXIOS
   // axios config for OpenAI
   const config = {
