8a7401a385cad0b88848bd5db3f2d3d7217e32f5
Kirsten Opstad <kirsten.opstad@gmail.com>
2023-03-08 11:43:01 -0800

    add Qs to FAQs

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,8 +1,26 @@
 import React from "react";
 
 const Faq = () => {
+  // how is artwork generated?
+  
+  // what is the tech stack used?
+  
+  // how did you come up with the prompts?
+  
+  // what are the future features or development plans?
+
+  // who owns this fake artwork?
+
+  // who owns this code?
+
+  // who owns anything?
+
+  // who are you, anyway?
+
   return(
-    <h1>Faq</h1>
+    <div className="faq">
+      <h1>Faq</h1>
+    </div>
   );
 }
 
