3c5de3400e45c0affd1fcf57c69dd0dffef787d7
Kirsten Opstad <kirsten.opstad@gmail.com>
2023-03-08 11:30:35 -0800

    add content to Membership and About

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,59 @@
 import React from "react";
+import Button from "react-bootstrap/Button"
+import Carousel from 'react-bootstrap/Carousel';
+import about from "./../img/about.png"
+import about1 from "./../img/about1.png"
+import about2 from "./../img/about2.png"
 
 const About = () => {
+  const prompt = "a photo of an entrance to a museum branded with the word MoFA"
   return(
-    <h1>About</h1>
+    <div className="about">
+      <Carousel>
+        <Carousel.Item>
+          <img src={about} alt={prompt} />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src={about1} alt={prompt} />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src={about2} alt={prompt} />
+        </Carousel.Item>
+      </Carousel>
+          <p>{prompt}</p>
+      <h1>About</h1>
+      <h2>What is MoFA?</h2>
+      <p>
+        MoFA (the Museum of Fake Art) is a culminating project demonstrating knowledge of full stack development. The project intergrates personal interests in art, creativity and criticism.
+      </p>
+      <p>
+        MoFA seeks to recreate the user experience of visiting a art musuem website with completely AI-generated artwork. By presenting AI-generated artwork in a format typically reserved for fine art, the application intends to provoke conversation around the following questions:
+      </p>
+      <ul>
+        <li>
+          what makes art "real" or "fake"?
+        </li>
+        <li>
+          is it possible to have a "real" human experiences with "fake" art?
+        </li>
+        <li>
+          what are the social / cultural / human implications of the development of generative AI?
+        </li>
+
+      </ul>
+      <h2>What Does it Do?</h2>
+      <p>
+        MoFa shows visitors works of curated fake art and invites to engage with the artwork by offering the ability to title artworks and vote on which titlethey like best.
+      </p>
+      <h2>Who is it For?</h2>
+      <p>
+        MoFA is for folks interested in the intersections of art, creativity and technology. 
+        <br />
+        It's for you! Do you like it? 
+        <Button variant="outline-dark" size="sm">yes</Button>
+        <Button variant="outline-dark" size="sm">no</Button>
+      </p>
+    </div>
   );
 }
 
--- src/components/Membership.js
+++ src/components/Membership.js
@@ -1,8 +1,19 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import patrons from "./../img/patrons.png"
 
 const Membership = () => {
+  // TODO: integrate with PayPal API or Patreon
+  const prompt = "a photo of two patrons in a gallery at a fake art museum daylight"
   return(
-    <h1>Membership</h1>
+    <div className="membership">
+      <img src={patrons} alt={prompt} />
+      <p>{prompt}</p>
+      <h1>Membership</h1>
+      <p>MoFA is a wep application built to provoke conversations around using AI to generate art.</p>
+      <p>Memberships support the ongoing development of fake art.</p>
+      <Button variant="outline-dark">Become a Member Today</Button>
+    </div>
   );
 }
 
