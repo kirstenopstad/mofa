0e293f905b9c76d776f0bf798c939dda9ea65454
Kirsten Opstad <kirsten.opstad@gmail.com>
2023-03-08 16:55:54 -0800

    add comment to voteResponse

--- src/components/Membership.js
+++ src/components/Membership.js
@@ -10,7 +10,7 @@ const Membership = () => {
       <img src={patrons} alt={prompt} />
       <p><i>{prompt}</i></p>
       <h1>Membership</h1>
-      <p>MoFA is a wep application built to provoke conversations around using AI to generate art.Memberships support the ongoing development of fake art.</p>
+      <p>MoFA is a wep application built to provoke conversations around using AI to generate art. Memberships support the ongoing development of fake art.</p>
       <Button variant="outline-dark">Become a Member Today</Button>
     </div>
   );
