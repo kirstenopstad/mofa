93e013c22e2bd4c0fdb80e32b0ffc786d18efc65
Kirsten Opstad <kirsten.opstad@gmail.com>
2023-03-08 20:14:28 -0800

    add responsive styling to PlanVisit

--- src/components/PlanVisit.js
+++ src/components/PlanVisit.js
@@ -1,27 +1,32 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const PlanVisit = () => {
   return(
-    <React.Fragment>
-      <div className="plan-visit">
-        <h1>Plan Your Visit</h1>
-        <div>
-          <h4>Hours</h4>
-          <p>T-F  10a-5p</p>
-          <p>SAT  9a-9p</p>
-          <p>SUN  9a-9p</p>
-          <p>CLOSED MONDAYS</p>
-        </div>
-        <div>
-          <h4>Getting Here</h4>
-          <p>OPEN YOUR MIND</p>
-        </div>
-        <div>
-          <h4>Buy Tickets</h4>
-          <p>FREE BUT AT WHAT COST</p>
-        </div>
-      </div>
-    </React.Fragment>
+    <Container className="plan-visit">
+      <Row>
+          <Col>
+            <h1>Plan Your Visit</h1>
+          </Col>
+          <Col>
+              <h4>Hours</h4>
+              <p>T-F  10a-5p</p>
+              <p>SAT  9a-9p</p>
+              <p>SUN  9a-9p</p>
+              <p>CLOSED MONDAYS</p>
+          </Col>
+          <Col>
+              <h4>Getting Here</h4>
+              <p>OPEN YOUR MIND</p>
+          </Col>
+          <Col>
+              <h4>Buy Tickets</h4>
+              <p>FREE BUT AT WHAT COST</p>
+          </Col>
+      </Row>
+    </Container>
   );
 }
 
