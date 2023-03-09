67d8bb12ff59dfd96939559f0465a8db2bcbef6b
Kirsten Opstad <kirsten.opstad@gmail.com>
2023-03-08 16:41:52 -0800

    add warhol & renoir to seed data

--- src/ExhSeedData.js
+++ src/ExhSeedData.js
@@ -4,11 +4,18 @@ import feininger from './img/gen_images/img-vujGHIheyP5efqoS37Urs2w3.png'
 import malibu from './img/gen_images/img-6Esjc3yYlUqZwh7rHlYicdjy.png'
 import bigSur from './img/gen_images/img-VFaITUC1FFtO8eyncpWBJJ12.png'
 import portland from './img/gen_images/img-FZALK1vPHqTaGxEtrkiqqgqO.png'
-
+import warhol1 from './img/gen_images/img-9Es9wzYwLVEnXmzC46GLln7Z.png'
+import warhol2 from './img/gen_images/img-AE0EegNHIfRsHCV4z8Ibf68l.png'
+import warhol3 from './img/gen_images/img-YC6tzwylSV6GC7NhgT43IK3T.png'
+import renoir1 from './img/gen_images/img-h5DVe19ZoJWs0zIUikVAWkjZ.png'
+import renoir2 from './img/gen_images/img-ibTXwK7qWcv82XcVFlH12wQ2.png'
+import renoir3 from './img/gen_images/img-hDkf1LWx0ixe4K2mVHIKSxem.png'
+import renoir4 from './img/gen_images/img-K171yV2R6StOfyfr6LyUEtd8.png'
+import renoir5 from './img/gen_images/img-4fF51x68u2u7EQXwwvD2IFik.png'
 
 const exhibitions = [
   { exhTitle: `Three of Four`,
-    endDate: `soon`,
+    endDate: `Soon`,
     description: `A poker game three ways`,
     id: 1
   },
@@ -16,6 +23,16 @@ const exhibitions = [
     endDate: `March 15`,
     description: `Expressionist landscapes`,
     id: 2
+  },
+  { exhTitle: `War Hall`,
+    endDate: `April 15`,
+    description: `Observe thrice daily`,
+    id: 3
+  },
+  { exhTitle: `Thought That Was The Other Guy`,
+    endDate: `May 15`,
+    description: `Portraits of American eats`,
+    id: 4
   },
 ]
 
@@ -49,6 +66,46 @@ const artworks = [
     prompt: `portland landscape in expressionist style`,
     exhId: 2,
     id: 6
+  },
+  { image: warhol1,
+    prompt: `purple toothpaste in the style of Andy Warhol`,
+    exhId: 3,
+    id: 8
+  },
+  { image: warhol2,
+    prompt: `purple toothpaste in the style of Andy Warhol`,
+    exhId: 3,
+    id: 9
+  },
+  { image: warhol3,
+    prompt: `purple toothpaste in the style of Andy Warhol`,
+    exhId: 3,
+    id: 10
+  },
+  { image: renoir1,
+    prompt: `painting of a diner at night in the style of Renoir`,
+    exhId: 4,
+    id: 11
+  },
+  { image: renoir2,
+    prompt: `painting of a diner at night in the style of Renoir`,
+    exhId: 4,
+    id: 12
+  },
+  { image: renoir3,
+    prompt: `painting of a diner at night in the style of Renoir`,
+    exhId: 4,
+    id: 13
+  },
+  { image: renoir4,
+    prompt: `painting of a diner at night in the style of Renoir`,
+    exhId: 4,
+    id: 14
+  },
+  { image: renoir5,
+    prompt: `painting of a diner at night in the style of Renoir`,
+    exhId: 4,
+    id: 15
   },
 ]
 
